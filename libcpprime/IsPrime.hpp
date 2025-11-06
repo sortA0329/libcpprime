2442b7d802d01efe8805009eeb6a51d6ff0b0790
Rac <rac75116@gmail.com>
2025-11-06 22:06:56 +0900

    Refactor IsPrime functions for improved clarity and performance

--- libcpprime/IsPrime.hpp
+++ libcpprime/IsPrime.hpp
@@ -43,6 +43,47 @@ LIBCPPRIME_CONSTEXPR inline bool IsPrime16(const std::uint64_t n) noexcept { ret
 constexpr const std::uint16_t Bases64[16384] = {
 #include "./internal/IsPrimeBases64.txt"
 };
+LIBCPPRIME_CONSTEXPR inline std::uint16_t GetBase(std::uint64_t x) noexcept { return Bases64[(0xad625b89u * static_cast<std::uint32_t>(x)) >> 18]; }
+LIBCPPRIME_CONSTEXPR inline bool IsPrime49(const std::uint64_t x) noexcept {
+    const MontgomeryModint64Impl<false> mint(x);
+    const std::int32_t S = CountrZero(x - 1);
+    const std::uint64_t D = (x - 1) >> S;
+    const auto one = mint.one();
+    const auto mone = mint.neg(one);
+    auto c = mint.raw(2);
+    auto d = mint.raw(GetBase(x));
+    auto a = c;
+    auto b = d;
+    if (D != 1) {
+        c = mint.mul(c, c);
+        d = mint.mul(d, d);
+        std::uint64_t ex = D >> 1;
+        while (ex != 1) {
+            const auto e = mint.mul(c, c);
+            const auto f = mint.mul(d, d);
+            if (ex & 1) {
+                a = mint.mul(a, c);
+                b = mint.mul(b, d);
+            }
+            c = e;
+            d = f;
+            ex >>= 1;
+        }
+    }
+    a = mint.mul(a, c);
+    b = mint.mul(b, d);
+    bool res1 = mint.same(a, one) || mint.same(a, mone);
+    bool res2 = mint.same(b, one) || mint.same(b, mone);
+    if (x % 4 == 1 && !(res1 && res2)) {
+        for (std::int32_t i = 0; i != S - 1; ++i) {
+            a = mint.mul(a, a);
+            b = mint.mul(b, b);
+            res1 |= mint.same(a, mone);
+            res2 |= mint.same(b, mone);
+        }
+    }
+    return res1 && res2;
+}
 template <bool Strict>
 LIBCPPRIME_CONSTEXPR inline bool IsPrime64(const std::uint64_t x) noexcept {
     const MontgomeryModint64Impl<Strict> mint(x);
@@ -50,93 +91,51 @@ LIBCPPRIME_CONSTEXPR inline bool IsPrime64(const std::uint64_t x) noexcept {
     const std::uint64_t D = (x - 1) >> S;
     const auto one = mint.one();
     const auto mone = mint.neg(one);
-    auto test2 = [=](std::uint64_t base1, std::uint64_t base2) -> bool {
-        auto c = mint.raw(base1);
-        auto d = mint.raw(base2);
-        auto a = c;
-        auto b = d;
-        if (D != 1) {
-            c = mint.mul(c, c);
-            d = mint.mul(d, d);
-            std::uint64_t ex = D >> 1;
-            while (ex != 1) {
-                const auto e = mint.mul(c, c);
-                const auto f = mint.mul(d, d);
-                if (ex & 1) {
-                    a = mint.mul(a, c);
-                    b = mint.mul(b, d);
-                }
-                c = e;
-                d = f;
-                ex >>= 1;
+    const std::uint32_t base = GetBase(x);
+    const std::uint64_t base_mask = 15ull | (135ull << 8) | (13ull << 16) | (60ull << 24) | (15ull << 32) | (117ull << 40) | (65ull << 48) | (29ull << 56);
+    auto d = mint.raw(2);
+    auto e = mint.raw(base);
+    auto f = mint.raw((base_mask >> (8 * (base >> 13))) & 0xff);
+    auto a = d;
+    auto b = e;
+    auto c = f;
+    if (D != 1) {
+        d = mint.mul(d, d);
+        e = mint.mul(e, e);
+        f = mint.mul(f, f);
+        std::uint64_t ex = D >> 1;
+        while (ex != 1) {
+            const auto g = mint.mul(d, d);
+            const auto h = mint.mul(e, e);
+            const auto i = mint.mul(f, f);
+            if (ex & 1) {
+                a = mint.mul(a, d);
+                b = mint.mul(b, e);
+                c = mint.mul(c, f);
             }
+            d = g;
+            e = h;
+            f = i;
+            ex >>= 1;
         }
-        a = mint.mul(a, c);
-        b = mint.mul(b, d);
-        bool res1 = mint.same(a, one) || mint.same(a, mone);
-        bool res2 = mint.same(b, one) || mint.same(b, mone);
-        if (x % 4 == 1 && !(res1 && res2)) {
-            for (std::int32_t i = 0; i != S - 1; ++i) {
-                a = mint.mul(a, a);
-                b = mint.mul(b, b);
-                res1 |= mint.same(a, mone);
-                res2 |= mint.same(b, mone);
-            }
-        }
-        return res1 && res2;
-    };
-    auto test3 = [=](std::uint64_t base1, std::uint64_t base2, std::uint64_t base3) -> bool {
-        auto d = mint.raw(base1);
-        auto e = mint.raw(base2);
-        auto f = mint.raw(base3);
-        auto a = d;
-        auto b = e;
-        auto c = f;
-        if (D != 1) {
-            d = mint.mul(d, d);
-            e = mint.mul(e, e);
-            f = mint.mul(f, f);
-            std::uint64_t ex = D >> 1;
-            while (ex != 1) {
-                const auto g = mint.mul(d, d);
-                const auto h = mint.mul(e, e);
-                const auto i = mint.mul(f, f);
-                if (ex & 1) {
-                    a = mint.mul(a, d);
-                    b = mint.mul(b, e);
-                    c = mint.mul(c, f);
-                }
-                d = g;
-                e = h;
-                f = i;
-                ex >>= 1;
-            }
-        }
-        a = mint.mul(a, d);
-        b = mint.mul(b, e);
-        c = mint.mul(c, f);
-        bool res1 = mint.same(a, one) || mint.same(a, mone);
-        bool res2 = mint.same(b, one) || mint.same(b, mone);
-        bool res3 = mint.same(c, one) || mint.same(c, mone);
-        if (x % 4 == 1 && !(res1 && res2 && res3)) {
-            for (std::int32_t i = 0; i != S - 1; ++i) {
-                a = mint.mul(a, a);
-                b = mint.mul(b, b);
-                c = mint.mul(c, c);
-                res1 |= mint.same(a, mone);
-                res2 |= mint.same(b, mone);
-                res3 |= mint.same(c, mone);
-            }
-        }
-        return res1 && res2 && res3;
-    };
-    const std::uint32_t base = Bases64[(0xad625b89u * static_cast<std::uint32_t>(x)) >> 18];
-    if (x < (1ull << 49)) {
-        return test2(2, base);
-    } else {
-        const std::uint64_t base2_mask = 15ull | (135ull << 8) | (13ull << 16) | (60ull << 24) | (15ull << 32) | (117ull << 40) | (65ull << 48) | (29ull << 56);
-        return test3(2, base, (base2_mask >> (8 * (base >> 13))) & 0xff);
     }
+    a = mint.mul(a, d);
+    b = mint.mul(b, e);
+    c = mint.mul(c, f);
+    bool res1 = mint.same(a, one) || mint.same(a, mone);
+    bool res2 = mint.same(b, one) || mint.same(b, mone);
+    bool res3 = mint.same(c, one) || mint.same(c, mone);
+    if (x % 4 == 1 && !(res1 && res2 && res3)) {
+        for (std::int32_t i = 0; i != S - 1; ++i) {
+            a = mint.mul(a, a);
+            b = mint.mul(b, b);
+            c = mint.mul(c, c);
+            res1 |= mint.same(a, mone);
+            res2 |= mint.same(b, mone);
+            res3 |= mint.same(c, mone);
+        }
+    }
+    return res1 && res2 && res3;
 }
 
 }  // namespace internal
@@ -150,6 +149,8 @@ LIBCPPRIME_CONSTEXPR inline bool IsPrime(std::uint64_t n) noexcept {
         }
         if (n <= 0xffffffff) {
             return internal::IsPrime32(static_cast<std::uint32_t>(n));
+        } else if (n < (std::uint64_t(1) << 49)) {
+            return internal::IsPrime49(n);
         } else if (n < (std::uint64_t(1) << 62)) {
             return internal::IsPrime64<false>(n);
         } else {
